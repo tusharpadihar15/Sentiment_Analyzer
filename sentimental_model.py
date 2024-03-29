9790d84689a6f1130738dbfc9601b3641b508cc4
Tushar Padihar <91619197+tusharpadihar15@users.noreply.github.com>
2024-03-30 03:46:53 +0530

    Update sentimental_model.py

--- sentimental_model.py
+++ sentimental_model.py
@@ -9,81 +9,74 @@ from sklearn.svm import SVC
 nltk.download('punkt')
 nltk.download('stopwords')
 
-# Load dataset from CSV file
+# Reading dataset
 data = pd.read_csv("hatedata.csv", encoding='latin')
 
-# Check the first few rows of the dataset
+# Printing first few rows
 print("Original Data:")
 print(data.head())
 
-
-# PREPROCESSING THE DATA 
+# Function for data preprocessing
 def preprocess_text(text):
   
-    # TOKENIZATION
+    # Tokenization
     tokens=word_tokenize(text)
 
-    # Convert tokens to lowercase
+    # To Lowercase
     tokens = [token.lower() for token in tokens]
     
     # Remove punctuation
     tokens = [token for token in tokens if token not in string.punctuation]
 
-    # STEMMING
+    # Stemming
     porter_stemmer=PorterStemmer()
     stemmed_tokens=[porter_stemmer.stem(token) for token in tokens]
 
-    # REMOVING STOP WORDS
+    # Removing stop words
     stop_words=set(stopwords.words('english'))
     filtered_tokens=[token for token in stemmed_tokens if token.lower() not in stop_words]
 
-    #JOINING THE TOKENS
+    #Joining tokens
     preprocess_text=' '.join(filtered_tokens)
 
     return preprocess_text
 
-
-# Preprocess the 'text' column in the dataset
+# Preprocessing the 'comment' column in the dataset
 data['clean_text'] = data['comment'].apply(preprocess_text)
 
-# Display the preprocessed data
+# Preprocessed data
 print("\nPreprocessed Data:")
 print(data.head())
 
-
+# Training and testing
 from sklearn.model_selection import train_test_split
-
 X = data['clean_text'].iloc[:1000]
 y = data['label'].iloc[:1000]
 
-# Split dataset into training and testing sets
+# Split dataset into training and testing sets as in ratio of 80:20
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
 
-
+# Vectorizing data
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score, classification_report
-
-# Vectorize text data using bag-of-words representation
 vectorizer = CountVectorizer()
 X_train_vectors = vectorizer.fit_transform(X_train)
 X_test_vectors = vectorizer.transform(X_test)
 
-
-classifier = SVC(kernel='linear')  # Use a linear kernel for SVM
+# Using SVM Model for prediction
+classifier = SVC(kernel='linear') 
 classifier.fit(X_train_vectors, y_train)
 
-
-# Save both the classifier and the vectorizer
+# Saving both the classifier and the vectorizer using joblib
 dump((classifier, vectorizer), 'model.joblib')
 
-
-# Predict on test set
+# Prediction on test set
 predictions = classifier.predict(X_test_vectors)
 
-
-# Evaluate model
+# Evaluation of model
 accuracy = accuracy_score(y_test, predictions)
 report = classification_report(y_test, predictions, zero_division=1)
 
+#Displaying accuracy and classification report which contains F1 score, recall, precision
 print("Accuracy:", accuracy)
 print("Classification Report:\n", report)
